70704c43bb8e21b4a9c0a86f6e4ac2811641b3ff
Billy Furie <bfurie7@gmail.com>
2019-06-19 19:18:48 -0400

    Added color change in navbar for the current page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function navBarTextColor() {
-    if (window.location.href == "index.html") {
+    if (window.location.pathname == "project0/index.html") {
         document.getElementById("navLogo").style.color = 'red';
     }
 }
+document.getElementById("navBar").innerHTML =
+    "Page path is " + window.location.pathname;
